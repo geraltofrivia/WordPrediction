381709375d6393d3ed6928c7d69ab22c9b2a92e7
geraltofrivia <devl.priyansh@gmail.com>
2015-04-29 16:52:28 +0530

    v0.3 Changed the file to something simple for better debugging The dictionary is now single level markov chainable. Need to assign weights. But right now I need to not assign weights but instead make two level markov chains

--- reader.py
+++ reader.py
@@ -4,18 +4,24 @@ Python 2.7'''
 import os
 
 corpus_dir =  os.path.join(os.path.dirname(__file__),'corpus')
-myfile = open(os.path.join(corpus_dir,'a.txt'), "r")
+myfile = open(os.path.join(corpus_dir,'a.txt'), "r").read().split()
 dictionary = {}
 
-for word in  myfile.read().split():
-	word = word.strip().lower()
-	if str.isalpha(word):
+#Filter myfile to store only words
+for word in myfile:
+	if not str.isalpha(word):
+		myfile.remove(word)
 
-		if not word in dictionary:
-			dictionary[word] = 1
-		else:
-			dictionary[word] += 1
 
+#Main thing
+for i in  range(1,len(myfile)):
+	word = myfile[i].strip().lower()
+	previousword = myfile[i-1].strip().lower()
+
+	if not previousword in dictionary:
+		dictionary[previousword] = [word]
+	else:
+		dictionary[previousword].append(word)
 
 print dictionary
 
